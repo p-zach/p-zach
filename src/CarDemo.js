8f0c781ebaf9ce648800c5d40c1f9aaf2202a9cf
p-zach <porterdzach@gmail.com>
2024-12-08 20:27:48 -0500

    Canvas testing

--- src/CarDemo.js
+++ src/CarDemo.js
@@ -1,7 +1,9 @@
+import { useState, useEffect } from 'react'
 import * as tf from '@tensorflow/tfjs';
 import Canvas from './components/Canvas';
 
 function CarDemo() {
+  const [resetKey, setResetKey] = useState(0);
 //   const model = tf.sequential();
 //   model.add(tf.layers.dense({units: 1, inputShape: [1]}));
 
@@ -18,16 +20,68 @@ function CarDemo() {
 //     // Open the browser devtools to see the output
 //   });
 
+  let pos = {x: 100, y: 100};
+  let speed = 3;  
+
+  let loaded = false;
+  const img = new Image();
+  img.onload = () => loaded = true;
+  img.crossOrigin = "Anonymous";
+  // img.src = './course1.png';
+  img.src = 'https://raw.githubusercontent.com/p-zach/self-driving-car/refs/heads/main/course1.png';
+
+  let prevPos = pos;
+
+  useEffect(() => {
+    // Add event listener for key presses
+    const handleKeyDown = (event) => {
+      prevPos = {x: pos.x, y: pos.y};
+      switch (event.key) {
+        case 'ArrowUp':
+          pos.y -= speed;
+          break;
+        case 'ArrowDown':
+          pos.y += speed;
+          break;
+        case 'ArrowLeft':
+          pos.x -= speed;
+          break;
+        case 'ArrowRight':
+          pos.x += speed;
+          break;
+        default:
+          break;
+      }
+    };
+
+    window.addEventListener('keydown', handleKeyDown);
+
+    return () => {
+      window.removeEventListener('keydown', handleKeyDown);
+    };
+  }, []);
+
   const draw = (ctx, frameCount) => {
     ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height)
-    ctx.fillStyle = '#000000'
+    // ctx.fillStyle = '#DDDDDD'
+    // ctx.fillRect(0, 0, ctx.canvas.width, ctx.canvas.height)
+    
+    if (loaded)
+      ctx.drawImage(img, 0, 0, 1500, 1000);
+    
+    const pixel = ctx.getImageData(pos.x, pos.y, 1, 1).data;
+    if (pixel[0] === 0 && pixel[1] === 0 && pixel[2] === 0)
+      pos = prevPos;
+
+    ctx.fillStyle = '#FF0000'
     ctx.beginPath()
-    ctx.arc(100, 100, 20*Math.sin(frameCount*0.05)**2, 0, 2*Math.PI)
+    ctx.arc(pos.x, pos.y, 20*Math.sin(frameCount*0.05)**2, 0, 2*Math.PI)
     ctx.fill()
   }
   
   return <div>
-    <Canvas draw={draw}/>
+    <button onClick={() => setResetKey(resetKey + 1)}>Reset</button>
+    <Canvas draw={draw} key={resetKey} width={1500} height={1000}/>
   </div>;
 }
 
